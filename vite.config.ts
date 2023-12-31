af59319ea7848e0823d57102a809c7fef20dfaf3
Shekhar Dutta <sherlockdoyle1904+github@gmail.com>
2023-12-31 21:13:33 +0530

    feat: add rollup-plugin-minify-html-literals

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,10 @@
+import minifyHTML from 'rollup-plugin-minify-html-literals';
 import { defineConfig } from 'vite';
 
-export default defineConfig({ base: '/bin-ocular' });
+export default defineConfig({
+  base: '/bin-ocular',
+  plugins: [
+    // @ts-ignore the default export is not exported in the type definitions
+    minifyHTML.default(),
+  ],
+});
